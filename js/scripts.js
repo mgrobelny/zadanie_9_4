b4c59d6fd118e047dfa32645556055fad65d732c
mgrobelny <31382587+mgrobelny@users.noreply.github.com>
2017-09-06 11:53:47 +0200

    unnecessary loop removed

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,6 @@ function drawTree(x) {
     for (var i = 1; i <= x; i++) {
         star = " ";
 
-        for (var j = x; j > i; j--) {
-            star = " ";
-
-        }
         for (j = 1; j <= i; j++) {
             star += "*";
 
